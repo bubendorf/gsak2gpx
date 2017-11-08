d698f91e46849ac039fc90c306034604bc84f666
Markus Bubendorf <markus@bubendorf.ch>
2017-11-08 22:36:57 +0100

    Caches schauen schon ganz gut aus!

--- creatCacheGPX.sh
+++ creatCacheGPX.sh
@@ -5,6 +5,12 @@ CAT_PATH=/Users/mbu/src/gsak2gpx/categories
 OUT_PATH=/Users/mbu/src/gsak2gpx/output
 TASKS=1
 CATEGORIES=ActiveCaches
+GGZ=ActiveCaches
 ENCODING=utf-8
+CACHES_PER_GPX=250
 
 java $OPTS -jar target/gsak2gpx-1.0-SNAPSHOT.jar --database $DB --categoryPath $CAT_PATH --categories $CATEGORIES --outputPath $OUT_PATH --tasks $TASKS --encoding $ENCODING
+
+rm -f $OUT_PATH/ggzgen/*.gpx
+java $OPTS -cp target/gsak2gpx-1.0-SNAPSHOT.jar ch.bubendorf.xmlsplit.XmlSplit --input $OUT_PATH/$GGZ.gpx --output $OUT_PATH/ggzgen --count $CACHES_PER_GPX --encoding $ENCODING
+/Users/mbu/src/ggz-tools/gpx2ggz.py $OUT_PATH/ggzgen/*.gpx $OUT_PATH/ggzgen/$GGZ.ggz
--- src/main/java/ch/bubendorf/gsak2gpx/SqlToGpx.java
+++ src/main/java/ch/bubendorf/gsak2gpx/SqlToGpx.java
@@ -7,12 +7,19 @@ import freemarker.template.Template;
 import freemarker.template.TemplateExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sqlite.Function;
+import org.sqlite.SQLiteConfig;
 
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import static freemarker.template.Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS;
 
@@ -40,7 +47,17 @@ public class SqlToGpx {
             Class.forName("org.sqlite.JDBC");
             LOGGER.info("Open " + database);
 //        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + database + ";Version=3;Pooling=True;Max Pool Size=20;Read Only=True;FailIfMissing=True;");
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + database);
+
+            SQLiteConfig config = new SQLiteConfig();
+            config.setReadOnly(true);
+            config.setSharedCache(true);
+            config.enableLoadExtension(true);
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + database, config.toProperties());
+            Function.create(connection, "sqrt", new Function() {
+            protected void xFunc() throws SQLException {
+                    result(Math.sqrt(value_double(0)));
+                }
+            });
 
             SqlTemplateMethod sqlTemplateMethod = new SqlTemplateMethod(connection);
             Configuration cfg = new Configuration(DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
@@ -55,6 +72,9 @@ public class SqlToGpx {
             rootModel.put("sql", sqlTemplateMethod);
             rootModel.put("category", category);
             rootModel.put("encoding", encoding);
+            rootModel.put("date", LocalDate.now());
+            rootModel.put("time", LocalTime.now());
+            rootModel.put("datetime", LocalDateTime.now());
 
             Template template = cfg.getTemplate(category + ".ftlx");
             Writer out = new OutputStreamWriter(new FileOutputStream(outputPath + "/" + category + ".gpx"), encoding);
