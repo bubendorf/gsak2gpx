1761a2e152d661e287ff39d37d4c0cbb2ac4cf70
Markus Bubendorf <markus@bubendorf.ch>
2017-11-16 20:23:00 +0100

    OtherCaches eingeführt. Namen der POI Kategorien schöner gemacht. Umlaute Problem behoben.

--- createCachePOIs.sh
+++ createCachePOIs.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-#OPTS="-Xmx6G"
+OPTS="-Xmx2G -Dorg.slf4j.simpleLogger.defaultLogLevel=info"
 DB=/Users/mbu/ExtDisk/Geo/GSAK8/data/Default/sqlite.db3
 CAT_PATH="/Users/mbu/src/gsak2gpx/categories/cachepoi /Users/mbu/src/gsak2gpx/categories/include"
 GPX_PATH=/Users/mbu/src/gsak2gpx/output/cachepoi
 OUT_PATH=/Users/mbu/src/gsak2gpx/output
 IMG_PATH=/Users/mbu/src/gsak2gpx/images/cachepoi
 TASKS=4
-CATEGORIES=Traditional,Unknown,Multi,VirtualCache,Earthcache,Wherigo,Webcam,Letterbox
+CATEGORIES=Traditional,Unknown,Multi,OtherCaches
+#CATEGORIES=OtherCaches
 # gpsbabel kommt NICHT mit utf-8 zurecht! Also nehmen wir halt das Windows-Zeugs!
-ENCODING=windows-1252
+# Valid values are windows-1250 to windows-1257.
+#ENCODING=windows-1252
+GPX_ENCODING=utf-8
+GPI_ENCODING=windows-1252
 
-java $OPTS -jar target/gsak2gpx-1.0.jar --database $DB --categoryPath $CAT_PATH --categories $CATEGORIES --outputPath $GPX_PATH --encoding $ENCODING --tasks $TASKS
+java $OPTS -jar target/gsak2gpx-1.0.jar --database $DB --categoryPath $CAT_PATH --categories $CATEGORIES --outputPath $GPX_PATH --encoding $GPX_ENCODING --tasks $TASKS
 
 # param 1: file
 # param 2: offset
@@ -27,7 +31,7 @@ function togpi {
   /bin/echo `gdate "+%Y-%m-%d %H:%M:%S:%3N"` Convert $1.gpx to $1.gpi
   START_TIME=`gdate +%s%N`
 #  echo Convert $1.gpx to $1.gpi
-  gpsbabel -i gpx -f $GPX_PATH/$1.gpx -o garmin_gpi,category="$3",bitmap=$IMG_PATH/$1.bmp,unique=0,writecodec=$ENCODING -F $OUT_PATH/$2.gpi
+  gpsbabel -i gpx -f $GPX_PATH/$1.gpx -o garmin_gpi,category="$3",bitmap=$IMG_PATH/$1.bmp,unique=0,writecodec=$GPI_ENCODING -F $OUT_PATH/$2.gpi
   replaceByte $OUT_PATH/$2.gpi 16 $4
   replaceByte $OUT_PATH/$2.gpi 17 $4
   STOP_TIME=`gdate +%s%N`
@@ -37,18 +41,18 @@ function togpi {
 
 function multigpi {
 # $1 Name der GPI Datei
-# $2.. Name der GPX und der BMP Dateien
-# $3.. Name der Kategorie
+# $2 Name der Kategorie
+# $3 Name BMP Datei
+# $4.. Namen der GPX Dateien
   /bin/echo `gdate "+%Y-%m-%d %H:%M:%S:%3N"` Converting to $1.gpi
   START_TIME=`gdate +%s%N`
   OUT="-F $OUT_PATH/$1.gpi"
   EXEC="gpsbabel -D 0"
-  for ((i=2;i<=$#;i+=2))
+  for ((i=4;i<=$#;i+2))
   do
-    let j=i+1
-    EXEC="$EXEC -i gpx -f $GPX_PATH/${!i}.gpx -o garmin_gpi,category=\"${!j}\",bitmap=$IMG_PATH/${!i}.bmp,unique=0,writecodec=$ENCODING"
+    EXEC="$EXEC -i gpx -f $GPX_PATH/${!i}.gpx"
   done
-  EXEC="$EXEC -F $OUT_PATH/$1.gpi"
+  EXEC="$EXEC -o garmin_gpi,category=$2,bitmap=$IMG_PATH/$3.bmp,unique=0,writecodec=$GPI_ENCODING -F $OUT_PATH/$1.gpi"
 #  echo $EXEC
   $EXEC
   STOP_TIME=`gdate +%s%N`
@@ -56,15 +60,15 @@ function multigpi {
   /bin/echo "($STOP_TIME-$START_TIME)/1000000" | bc
 }
 
-togpi Traditional 20-Traditional Traditional 20 &
-togpi Unknown 22-Unknown Unknown 22 &
-togpi Multi 21-Multi Multi 21 &
-sleep 2
-togpi VirtualCache 23-VirtualCache VirtualCache 23 &
-togpi Letterbox 24-Letterbox Letterbox 24 &
-togpi Earthcache 25-Earthcache Earthcache 25 &
-togpi Wherigo 26-Wherigo Wherigo 26 &
-togpi Webcam 27-Webcam Webcam 27 &
-#multigpi 29-CachePOI VirtualCache VirtualCache Letterbox Letterbox Earthcache Earthcache Wherigo Wherigo Webcam Webcam &
+togpi Traditional 20-Traditional "Traditional Cache" 20 &
+togpi Unknown 22-Unknown "Unknown Cache" 22 &
+togpi Multi 21-Multi "Multi Cache" 21 &
+togpi OtherCaches 29-OtherCaches "Other Caches" 29 &
+#togpi VirtualCache 23-VirtualCache VirtualCache 23 &
+#togpi Letterbox 24-Letterbox Letterbox 24 &
+#togpi Earthcache 25-Earthcache Earthcache 25 &
+#togpi Wherigo 26-Wherigo Wherigo 26 &
+#togpi Webcam 27-Webcam Webcam 27 &
+#multigpi 29-OtherCaches OtherCaches OtherCaches VirtualCache Letterbox Earthcache Wherigo Webcam &
 
 wait
--- createPOIs.sh
+++ createPOIs.sh
@@ -7,10 +7,13 @@ OUT_PATH=/Users/mbu/src/gsak2gpx/output
 IMG_PATH=/Users/mbu/src/gsak2gpx/images/gpigen
 TASKS=4
 CATEGORIES=Favorites,Parking,Virtual,HasParking,Reference,Trailhead,Simple,Physical,Original,Final,Disabled,Corrected,Terrain5
+#CATEGORIES=HasParking
 # gpsbabel kommt NICHT mit utf-8 zurecht! Also nehmen wir halt das Windows-Zeugs!
-ENCODING=windows-1252
+# Valid values are windows-1250 to windows-1257.
+GPX_ENCODING=utf-8
+GPI_ENCODING=windows-1252
 
-java $OPTS -jar target/gsak2gpx-1.0.jar --database $DB --categoryPath $CAT_PATH --categories $CATEGORIES --outputPath $GPX_PATH --encoding $ENCODING --tasks $TASKS
+java $OPTS -jar target/gsak2gpx-1.0.jar --database $DB --categoryPath $CAT_PATH --categories $CATEGORIES --outputPath $GPX_PATH --encoding $GPX_ENCODING --tasks $TASKS
 
 # param 1: file
 # param 2: offset
@@ -26,8 +29,8 @@ function togpi {
 # $4 Time Offset, used to create unique GPI identifiers
   /bin/echo `gdate "+%Y-%m-%d %H:%M:%S:%3N"` Convert $1.gpx to $1.gpi
   START_TIME=`gdate +%s%N`
-#  gpsbabel -i gpx -f $GPX_PATH/$1.gpx -o garmin_gpi,category="$3",bitmap=$IMG_PATH/$1.bmp,unique=0,writecodec=$ENCODING,notes,descr -F $OUT_PATH/$2.gpi
-  gpsbabel -i gpx -f $GPX_PATH/$1.gpx -o garmin_gpi,category="$3",bitmap=$IMG_PATH/$1.bmp,unique=0,writecodec=$ENCODING -F $OUT_PATH/$2.gpi
+#  gpsbabel -i gpx -f $GPX_PATH/$1.gpx -o garmin_gpi,category="$3",bitmap=$IMG_PATH/$1.bmp,unique=0,writecodec=$GPI_ENCODING,notes,descr -F $OUT_PATH/$2.gpi
+  gpsbabel -i gpx -f $GPX_PATH/$1.gpx -o garmin_gpi,category="$3",bitmap=$IMG_PATH/$1.bmp,unique=0,writecodec=$GPI_ENCODING -F $OUT_PATH/$2.gpi
   replaceByte $OUT_PATH/$2.gpi 16 $4
   replaceByte $OUT_PATH/$2.gpi 17 $4
   STOP_TIME=`gdate +%s%N`
@@ -46,7 +49,7 @@ function multigpi {
   for ((i=2;i<=$#;i+=2))
   do
     let j=i+1
-    EXEC="$EXEC -i gpx -f $GPX_PATH/${!i}.gpx -o garmin_gpi,category=\"${!j}\",bitmap=$IMG_PATH/${!i}.bmp,unique=0,writecodec=$ENCODING"
+    EXEC="$EXEC -i gpx -f $GPX_PATH/${!i}.gpx -o garmin_gpi,category=\"${!j}\",bitmap=$IMG_PATH/${!i}.bmp,unique=0,writecodec=$GPI_ENCODING"
   done
   EXEC="$EXEC -F $OUT_PATH/$1.gpi"
 #  echo $EXEC
@@ -59,15 +62,15 @@ function multigpi {
 # multigpi 99-Attributes.gpi Favorites A-Favoriten Simple A-Simple HasParking A-HasParking Corrected A-Corrected Terrain5 A-Terrain5 Disabled A-Disabled
 
 togpi HasParking 52-Attr-HasParking A-HasParking 52 &
-togpi Parking 35-Parking Parking 35 &
+togpi Parking 35-Parking "Parking Place" 35 &
 togpi Favorites 50-Attr-Favorites A-Favoriten 50 &
 togpi Simple 51-Attr-Simple A-Simple 51 &
-togpi Virtual 11-Virtual Virt-Stage 11 &
+togpi Virtual 11-Virtual "Virtual Stage" 11 &
 togpi Corrected 53-Attr-Corrected A-Corrected 53 &
-togpi Original 34-Original Original 34 &
-togpi Reference 33-Reference Ref-Point 33 &
+togpi Original 34-Original "Original Coordinats" 34 &
+togpi Reference 33-Reference "Reference Point" 33 &
 togpi Trailhead 32-Trailhead Trailhead 32 &
-togpi Physical 10-Physical Phys-Stage 10 &
+togpi Physical 10-Physical "Physical Stage" 10 &
 togpi Terrain5 54-Attr-Terrain5 A-Terrain5 54 &
 togpi Disabled 55-Attr-Disabled A-Disabled 55 &
 togpi Final 31-Final Final 31 &
