5d469ec0524707612c23533306924d08ffef44d1
Markus Bubendorf <markus@bubendorf.ch>
2018-04-08 21:09:11 +0200

    RUPI Export für Waypoints

--- createRUPI.sh
+++ createRUPI.sh
@@ -8,36 +8,100 @@ export OPTS="-XX:+UseParallelGC -Xmx1500M -Dorg.slf4j.simpleLogger.defaultLogLev
 export ENCODING=iso8859-1
 export CAT_PATH="$BASE/categories/rupi $BASE/categories/include"
 
-function createRupi() {
-# $1 Name der Kategorie(-n)
+function createCSV() {
+# $1 Name der Kategorie
 # $2 Land
 # $3 0=Enabled, 1=Disabled, Leer=Egal
 # $4 0=Ohne Corrected Coordinates, 1=Mit Corrected Coordinates, Leer=Egal
 # $5 Suffix der erzeugten Datei
 # $6 Extension der erzeugten Datei
-  java $OPTS -jar $JAR --database $DB --categoryPath $CAT_PATH --categories $1 --outputPath $OUT_PATH/rupi --outputFormat plainText --suffix $5 --extension $6 --param country=$2 disabled=$3 corrected=$4 --encoding $ENCODING
+  java $OPTS -jar $JAR --database $DB --categoryPath $CAT_PATH --categories $1 --outputPath $CSV_PATH --outputFormat plainText --suffix $5 --extension $6 --param country=$2 disabled=$3 corrected=$4 --encoding $ENCODING
+  FILENAME=$CSV_PATH/$5$1$6
+#  echo $FILENAME
+  actualsize=$(stat -c%s $FILENAME)
+  minimumsize=15
+  if [ $actualsize -lt $minimumsize ]
+  then
+    echo "File $FILENAME is too small ==> Deleting"
+    rm $FILENAME
+  fi
 }
-export -f createRupi
+export -f createCSV
+
+#createCSV Parking Switzerland "" "" CH_ _Parking.csv
+#createCSV Traditional France  $1 0 1 FR_ _Corr.csv
+#exit 0
 
 function createCountry() {
 # $1 Land (Switzerland, Germany, etc.)
 # $2 Kürzel des Landes (CH, DE, etc.)
-  createRupi Traditional $1 0 "" $2_ .csv
-  createRupi Traditional $1 0 1 $2_ _Corr.csv
-  createRupi Traditional $1 1 "" $2_ _Disa.csv
-  createRupi Multi $1 0 "" $2_ .csv
-  createRupi Multi $1 0 1 $2_ _Corr.csv
-  createRupi Multi $1 1 "" $2_ _Disa.csv
-  createRupi Unknown $1 0 "" $2_ .csv
-  createRupi Unknown $1 0 1 $2_ _Corr.csv
-  createRupi Unknown $1 1 "" $2_ _Disa.csv
+  createCSV Parking $1 "" "" $2_ .csv
+  createCSV Traditional $1 0 "" $2_ .csv
+  createCSV Traditional $1 0 1 $2_ _Corr.csv
+  createCSV Traditional $1 1 "" $2_ _Disa.csv
+  createCSV Multi $1 0 "" $2_ .csv
+  createCSV Multi $1 0 1 $2_ _Corr.csv
+  createCSV Multi $1 1 "" $2_ _Disa.csv
+  createCSV Unknown $1 0 "" $2_ .csv
+  createCSV Unknown $1 0 1 $2_ _Corr.csv
+  createCSV Unknown $1 1 "" $2_ _Disa.csv
 }
 export -f createCountry
 
-# parallel --delay 1 -j $TASKS -u doit ::: E8_Nord E7_Nord E5_6 E9_10 E7_8_Sued
-# parallel --delay 1 -j $TASKS -u doit ::: UserFlagOst UserFlagWest
+function convertToRupi() {
+# $1 Name der Kategorie bzw. des Teilnames der Dateien
+  java -jar $RUPI_JAR -n $1 -o $RAWRUPI_PATH $CSV_PATH/*$1.csv
+  cp $ICON_PATH/$1.bmp $RAWRUPI_PATH
+}
+export -f convertToRupi
 
-parallel --delay 1 -j $TASKS -u createCountry ::: Switzerland Germany France :::+ CH DE FR
-#createCountry Switzerland CH
-#createCountry Germany DE
-#createCountry France FR
+#parallel --delay 1 -j $TASKS -u createCountry ::: Switzerland Germany France Netherlands Liechtenstein Austria Italy :::+ CH DE FR NL LI AT IT
+#parallel -j $TASKS -u convertToRupi ::: Parking Traditional Traditional_Corr Traditional_Disa Multi Multi_Corr Multi_Disa Unknown Unknown_Corr Unknown_Disa
+
+mkdir -p $RUPI_PATH/aut
+rm -f $RUPI_PATH/aut/*
+ln $RAWRUPI_PATH/AT_* $RUPI_PATH/aut
+
+mkdir -p $RUPI_PATH/che
+rm -f $RUPI_PATH/che/*
+ln $RAWRUPI_PATH/CH_* $RUPI_PATH/che
+
+mkdir -p $RUPI_PATH/deu01
+rm -f $RUPI_PATH/deu01/*
+ln $RAWRUPI_PATH/DE_* $RUPI_PATH/deu01
+
+mkdir -p $RUPI_PATH/deu03
+rm -f $RUPI_PATH/deu03/*
+ln $RAWRUPI_PATH/DE_* $RUPI_PATH/deu03
+
+mkdir -p $RUPI_PATH/fra01
+rm -f $RUPI_PATH/fra01/*
+ln $RAWRUPI_PATH/FR_* $RUPI_PATH/fra01
+
+mkdir -p $RUPI_PATH/fra07
+rm -f $RUPI_PATH/fra07/*
+ln $RAWRUPI_PATH/FR_* $RUPI_PATH/fra07
+
+mkdir -p $RUPI_PATH/fra08
+rm -f $RUPI_PATH/fra08/*
+ln $RAWRUPI_PATH/FR_* $RUPI_PATH/fra08
+
+mkdir -p $RUPI_PATH/ita02
+rm -f $RUPI_PATH/ita02/*
+ln $RAWRUPI_PATH/IT_* $RUPI_PATH/ita02
+
+mkdir -p $RUPI_PATH/ita03
+rm -f $RUPI_PATH/ita03/*
+ln $RAWRUPI_PATH/IT_* $RUPI_PATH/ita03
+
+mkdir -p $RUPI_PATH/lie
+rm -f $RUPI_PATH/lie/*
+ln $RAWRUPI_PATH/LI_* $RUPI_PATH/lie
+
+mkdir -p $RUPI_PATH/nid
+rm -f $RUPI_PATH/nid/*
+ln $RAWRUPI_PATH/NL_* $RUPI_PATH/nid
+
+#convertToRupi Traditional
+#convertToRupi Traditional_Corr
+#convertToRupi Traditional_Disa
--- env.sh
+++ env.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-if [ -f /home/mbu/GSAK8/data/Default/sqlite.db3 ] 
+if [ -f /home/mbu/GSAK8/data/Default/sqlite.db3 ]
 then
 # Seems we are on N042
   export BASE=/home/mbu/src/gsak2gpx
-  export DB=/home/mbu/GSAK8/data/Default/sqlite.db3 
+  export DB=/home/mbu/GSAK8/data/Default/sqlite.db3
   export SQL_EXT=/home/mbu/src/gsak2gpx/lib/libsqlitefunctions
   DATE=date
   TASKS=2
 else
 # Seems we are on the Mac
   export BASE=/Users/mbu/src/gsak2gpx
-  export DB=/Users/mbu/ExtDisk/Geo/GSAK8/data/Default/sqlite.db3 
+  export DB=/Users/mbu/ExtDisk/Geo/GSAK8/data/Default/sqlite.db3
   export SQL_EXT=/Users/mbu/src/gsak2gpx/lib/libsqlitefunctions.dylib
   DATE=gdate
   TASKS=4
-fi 
+fi
 
-export CAT_PATH="$BASE/categories/ggz $BASE/categories/cachepoi $BASE/categories/attributepoi $BASE/categories/include" 
+export CAT_PATH="$BASE/categories/ggz $BASE/categories/cachepoi $BASE/categories/attributepoi $BASE/categories/include"
 export OUT_PATH=$BASE/output
 export GPX_PATH=$OUT_PATH/cachepoi
+export RAWRUPI_PATH=$OUT_PATH/rawrupi
+export RUPI_PATH=$OUT_PATH/rupi
+export CSV_PATH=$OUT_PATH/csv
 export JAR=target/gsak2gpx-1.2.jar
+export RUPI_JAR=../rupi/target/rupi-1.0-SNAPSHOT-jar-with-dependencies.jar
+export ICON_PATH=/Users/mbu/ExtDisk/Geo/GSAK8/Sygic/Icons
 
 mkdir -p $OUT_PATH
 mkdir -p $GPX_PATH
+mkdir -p $RUPI_PATH
+mkdir -p $RAWRUPI_PATH
+mkdir -p $CSV_PATH
